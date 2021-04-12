7a33058c1f9a7056f41363752b0a7552b58fce07
ianna.gico@gmail.com <ianna.gico.silva@avanade.com>
2021-04-12 12:03:15 -0300

    Atualizando models e relacionamentos

--- instagram.js
+++ instagram.js
@@ -1,4 +1,75 @@
 const {Usuario, Comentario, Post, sequelize} = require('./models');
+const { Op } = require('sequelize');
+
+Usuario.destroy({
+    where: {
+        id: 3
+    }
+}).then((resultado) => {
+    console.log(resultado);
+})
+
+
+Usuario.update({
+    email: 'sergio@digitalhouse.com'
+}, {
+    where: {
+        id: 2
+    }
+}).then((resultado) => {
+    console.log(resultado);
+});
+
+
+Usuario.create({
+
+        nome: 'Stefani',
+        email: 'stafani@digitalhouse.com',
+        senha: '1234'
+   
+    }).then((resultado) => {
+        console.log(resultado.toJSON());
+     });
+
+     Post.create({
+         texto: 'eu consegui',
+         usiarios_id: 11
+     }).then((resultado) => {
+        console.table(resultado.toJSON());
+     });
+
+Usuario.findAll({
+    where: {
+        nome: {[Op.like]: '%A%' }
+    }
+})
+.then((resultado) => {
+    console.table(resultado.map(post  => post.toJSON()));
+});
+
+Usuario.findAll({
+    where: {
+        nome: {[Op.notLike]: '%A%' }
+    }
+})
+.then((resultado) => {
+    console.table(resultado.map(post  => post.toJSON()));
+});
+
+var i = 0;
+do{
+Comentario.findAll({
+            order: [
+            ['id']
+        ], 
+         offset: i,
+         limit: 2,
+    })
+    .then((resultado) => {
+        console.table(resultado.map(post  => post.toJSON()));    
+})
+i += 2;
+}while(i<6);
 
 Usuario.findAll()
 .then((resultado) => {
